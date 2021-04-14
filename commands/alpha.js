ab55ca4bd7b469fb498ee81289bed796d3452318
DarrienGopaul <47226430+DarrienGopaul@users.noreply.github.com>
2021-04-14 02:10:16 -0400

    Delete alpha.js

--- commands/alpha.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    name: 'alpha',
-    description: "Funny tyler1 reference",
-    execute(beepBop, message, args) {
-        message.channel.send("GET THIS SHIT OFF MEEEEEEEEEEEEEEEEEEEEEEEEEEEE\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    }
-}
